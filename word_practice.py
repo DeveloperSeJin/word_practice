4b3353c1ff791ed3b706c42fc76cf0237b27c919
sejin <sejinp98@gmail.com>
2023-11-24 00:51:22 +0900

    ui_update

--- word_practice.py
+++ word_practice.py
@@ -5,20 +5,23 @@ import platform
 
 def practice(df, cl):
     review_index = []
+
     for idx, row in df.iterrows():
         os.system(cl)
         print('[' + str(idx+1) + '] '+ row['단어'])
         word = [row['뜻1'],row['뜻2'],row['뜻3'],row['뜻4'],row['그외']]
         means = input('뜻을 입력: ')
         print()
-        print([ w for w in word if str(w) != 'nan'])
+        print([ w for w in word if str(w) != ''])
         print(means)
         while True:
             try:
                 answer = input('맞았습니까?(0/1/종료=q): ')
                 if answer == 'q':
-                    return []
+                    return review_index
                 elif int(answer) == 1 or int(answer) == 0:
+                    if not int(answer):
+                        review_index.append(idx)
                     break
                 else:
                     raise
@@ -28,13 +31,13 @@ def practice(df, cl):
         print()
         print()
 
-
-        if not answer:
-            review_index.append(idx)
     os.system(cl)
-    print('맞은 개수: ', (len(df)-len(review_index)))
-    print('틀은 개수: ', len(review_index))
-    print(df.loc[review_index])
+    if len(review_index) == 0:
+        print('모든 단어를 맞췄습니다.')
+    else:
+        print('맞은 개수: ', (len(df)-len(review_index)))
+        print('틀은 개수: ', len(review_index))
+        print(df.loc[review_index])
     print()
     print()
 
@@ -42,6 +45,7 @@ def practice(df, cl):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('path', help='파일의 경로를 적으시오')
     parser.add_argument('Day', help='공부할 일차를 적으시오')
     args = parser.parse_args()
     
@@ -51,21 +55,32 @@ if __name__ == '__main__':
         case 'Linux':
             clear = 'clear'
             
-    words = pd.read_excel('./toefl.xlsx', engine='openpyxl', sheet_name=args.Day)
+    words = pd.read_excel(args.path, engine='openpyxl', sheet_name=args.Day)
+    words = words.fillna('')
     idx = practice(words, clear)
 
     while True:
         try:
-            retry = int(input('종료/처음부터/틀린부분만 재시작(0/1/2): '))
-            match retry:
-                case 0:
-                    break
-                case 1:
-                    idx = practice(words, clear)
-                case 2:
-                    idx = practice(words.loc[idx], clear)
-                case _:
-                    print('0/1/2 중 하나를 입력하시오.')
+            if len(idx) == 0:
+                retry = int(input('종료/처음부터(0/1): '))
+                match retry:
+                    case 0:
+                        break
+                    case 1:
+                        idx = practice(words, clear)
+                    case _:
+                        print('0/1 중 하나를 입력하시오.')
+            else:
+                retry = int(input('종료/처음부터/틀린 단어 복습(0/1/2): '))
+                match retry:
+                    case 0:
+                        break
+                    case 1:
+                        idx = practice(words, clear)
+                    case 2:
+                        idx = practice(words.loc[idx], clear)
+                    case _:
+                        print('0/1/2 중 하나를 입력하시오.')
         except:
             print('숫자를 입력하시오.')
         print()
